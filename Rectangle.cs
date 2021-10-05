656d51f3a6204f7e785bc63bf98aed788200dce2
samveg <vyassamveg123@gmail.com>
2021-10-05 00:06:48 -0400

    Add project files.

---
+++ Rectangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samveg_Vyas_8737672
+{
+    public class Rectangle
+    {
+        private int length;
+        private int width;
+
+        public int Length { get => length; set => length = value; }
+        public int Width { get => width; set => width = value; }
+
+        public int GetLength()
+        {
+            return Length;
+        }
+        public int SetLength(int length)
+        {
+            return length;
+        }
+        public int GetWidth()
+        {
+            return Width;
+        }
+        public int SetWidth(int width)
+        {
+            return width;
+        }
+
+
+        public int GetPerimeter()
+        {
+            return (2*(Length+Width));
+        }
+        public int GetArea()
+        {
+            return Length * Width;
+        }
+
+        public Rectangle()
+        {
+            Length = 1;
+            Width = 1;
+        }
+
+        public Rectangle(int length, int width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+    }
+}
